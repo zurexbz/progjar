6564286d90dbefb5921e207aee4db157953559ea
Mohammad Firman Fardiansyah <83849481+zurexbz@users.noreply.github.com>
2023-07-04 21:22:14 +0700

    Create runserver_process.sh

---
+++ tugas5/runserver_process.sh
@@ -0,0 +1,5 @@
+python3 server_process_pool_http.py 8002 &
+python3 server_process_pool_http.py 8003 &
+python3 server_process_pool_http.py 8004 &
+python3 server_process_pool_http.py 8005 &
+python3 server_process_pool_http.py 8006
